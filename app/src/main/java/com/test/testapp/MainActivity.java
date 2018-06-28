1163c3de89e179dd44f7ecd43922b8034c053573
Vitaliy Movchan <mvv.original@gmail.com>
2018-06-28 20:06:42 +0300

    added master lib

--- app/src/main/java/com/test/testapp/MainActivity.java
+++ app/src/main/java/com/test/testapp/MainActivity.java
@@ -3,11 +3,14 @@ package com.test.testapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.mylibrary.Feature;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Feature.like();
     }
 }
